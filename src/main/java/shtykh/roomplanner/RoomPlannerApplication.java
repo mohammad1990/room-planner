a3bea8bb4b34fc0e0c89495d0df8378c5148e4dd
Alexey Shtykh <alexey.sa@alibaba-inc.com>
2018-10-03 13:20:13 +0300

    init a project

---
+++ src/main/java/shtykh/roomplanner/RoomPlannerApplication.java
@@ -0,0 +1,12 @@
+package shtykh.roomplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomPlannerApplication.class, args);
+	}
+}
